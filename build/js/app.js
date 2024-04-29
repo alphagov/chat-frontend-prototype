0e8a6dbe9ae9d0ad79d51ada8a8a29894eced198
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2024-04-29 16:50:27 +0100

    Add `aria-busy`

--- build/js/app.js
+++ build/js/app.js
@@ -4,7 +4,12 @@
   const hiddenMessagesGroup3 = document.querySelectorAll("[data-group='3'] .message, [data-group='3'] .form-group");
   const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
-  function showChatMessages(messages) {
+  function showChatMessages(messages, groupIndex) {
+    // get parent wrapper for each message group
+    let messageGroup = document.querySelector(`[data-group='${groupIndex}']`);
+    // let messageGroup = document.querySelector(".messages__wrapper");
+    messageGroup.setAttribute("aria-busy", true);
+
     function showChatMessageWithDelay(message, delay) {
       return new Promise((resolve) => {
         setTimeout(() => {
@@ -19,6 +24,13 @@
       setTimeout(async () => {
         await showChatMessageWithDelay(message, delay);
         scrollToMessages(message);
+        if (index === messages.length - 1) {
+          // messages
+          //   .item(messages.length - 1)
+          //   .querySelector("button")
+          //   .focus();
+          messageGroup.setAttribute("aria-busy", false);
+        }
       }, delay);
     });
   }
@@ -31,19 +43,19 @@
     }
   }
 
-  showChatMessages(hiddenMessagesGroup1);
+  showChatMessages(hiddenMessagesGroup1, 1);
 
   document.querySelector("#iUnderstand").addEventListener("click", () => {
     document.querySelector("[data-group='1'] .button-group").classList.remove("is-visible");
     document.querySelector(".author-message.understand").classList.add("is-visible");
-    showChatMessages(hiddenMessagesGroup2);
+    showChatMessages(hiddenMessagesGroup2, 2);
     // scrollToMessages(hiddenMessagesGroup2[0]);
   });
 
   document.querySelector("#beginChat").addEventListener("click", () => {
     document.querySelector("[data-group='2'] .button-group").classList.remove("is-visible");
     document.querySelector(".author-message.okay").classList.add("is-visible");
-    showChatMessages(hiddenMessagesGroup3);
+    showChatMessages(hiddenMessagesGroup3, 3);
     // scrollToMessages(hiddenMessagesGroup3[0]);
   });
 
